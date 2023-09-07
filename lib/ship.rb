c44da1a789f2f79e422895cd8ef6aab129e1afb7
Anthea Yur <acyur6@gmail.com>
2023-09-07 17:50:29 -0500

    Ship: Add method hit, might need to refactor depending on if health should be built into innitiaize or its own method

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,10 +1,17 @@
+
 class Ship
-  attr_reader :name :length
+  attr_reader :name, :length
   def initialize(name,length)
     @name = name
     @length = length
+    @health = length
+  end
+
+  def hit
+    @health-=1
   end
 
 
-
 end
+
+require 'pry'; binding.pry
