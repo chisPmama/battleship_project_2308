43ee9570adc1a4648718ab8a0dce8243551e635f
Victor Sanchez <vsanantone@gmail.com>
2023-09-07 18:51:28 -0500

    test/cell: add initialize test for cell_spec file

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,1 +1,21 @@
-"Practice"
+require './lib/ship'
+require './lib/cell'
+
+RSpec.describe Cell do
+  before(:each) do
+    @cell = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  describe "#initialize" do
+    it "exist" do
+      expect(@cell).to be_a(Cell)
+    end
+
+    it "has a coordinate and a ship" do
+      expect(@cell.coordinate).to eq("B4")
+      expect(@cell.ship).to eq(nil)
+    end
+  end
+
+end
