55528f68952f3d9eef3c7ab1b9c927b4304ff644
Anthea Yur <acyur6@gmail.com>
2023-09-08 12:36:04 -0500

    feat/board: Create new spec and ruby file for board feature

---
+++ spec/board_spec.rb
@@ -0,0 +1,14 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+RSpec.describe Board do
+  before(:each) do
+    @board = Board.new
+  end
+
+  it 'exists' do
+    
+  end
+
+end
