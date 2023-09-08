b956137acf8a1d2f19546a43255dd76cfee89c06
Anthea Yur <acyur6@gmail.com>
2023-09-08 15:28:24 -0500

    feat/board: Expanded class scope to include cells hash for valid_coordinate method due to dynamic cells method

--- lib/board.rb
+++ lib/board.rb
@@ -1,21 +1,28 @@
 require './lib/cell'
 class Board
+  attr_reader :cells 
+
+  def initialize
+    @cells = {}
+  end
 
   def cells(h = 4, w = 4)
-    cells = {}
     height = "A"
     h.times do
       width = 1
       w.times do
         cell_name = height + (width).to_s #A2
-        cells[cell_name]=Cell.new(cell_name)
+        @cells[cell_name]=Cell.new(cell_name)
         width+=1
       end
       height = height.next
     end
-    cells
+    @cells
   end
 
+  def valid_coordinate?(coordinate)
+    
+  end
 end
 
 # require 'pry'; binding.pry
