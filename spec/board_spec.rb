375e1df1845d8a6f13df19f74c674e1c5943b4a2
Anthea Yur <acyur6@gmail.com>
2023-09-08 12:49:19 -0500

    test/board: Finish testing for board cells

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,10 +5,18 @@ require './lib/board'
 RSpec.describe Board do
   before(:each) do
     @board = Board.new
-  end
-
-  it 'exists' do
     
   end
 
+  it 'exists' do
+    expect(@board).to be_a(Board)
+  end
+
+  it 'defaults to a 4x4 board upon creation' do
+    @board.cells
+    expect(@board.cells).to eq(Hash)
+    expect(@board.cells.count).to eq(16)
+  end
+
+  
 end
