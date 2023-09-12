bae5cfa02279188ba155c2c0630bf5c8a5afdde7
Anthea Yur <acyur6@gmail.com>
2023-09-12 15:09:15 -0500

    feat/runner: Change name of file, tabling the concept of Battleship class for now, want to write functional script for game first

--- lib/runner.rb
+++ lib/runner.rb
@@ -7,25 +7,26 @@ require 'pry'
 #Computer has x amount of ships with x amount of health and so does user
 #logic shows that the game is over when health of all ships is 0
 
+## INTRODUCTION/BOARD CREATION
 system("clear")
 puts"Welcome to BATTLESHIP"
 puts "Enter p to play. Enter q to quit."
-
 play_mode = gets.chomp
 if play_mode == 'p'
   system("clear")
   puts "Let's play Battleship!\nWhat size board do you want to play with? I.E. 4x4\nOtherwise, press ENTER for default map."
-  dimensions = gets
+  input_board_dimensions = gets
   board = Board.new
-  if dimensions == "\n"
+  if input_board_dimensions == "\n"
      board.board_cells
   else
-    dimensions = (dimensions.chomp.sub("x"," ").split).map {|str| str.to_i}
-  board.board_cells(dimensions[0],dimensions[1])
+    input_board_dimensions = (input_board_dimensions.chomp.sub("x"," ").split).map {|str| str.to_i}
+  board.board_cells(input_board_dimensions[0],input_board_dimensions[1])
   end
 
+## BUILDING USER BOARD WITH CRUISER
   system("clear")
-  puts "I have laid out my ships on the grid.\nYou now need to lay out your two ships.\nThe Cruiser is three units long and the Submarine is two units long."
+  puts "I have laid out my ships on the grid.\nYou now need to lay out your two ships.\nThe Cruiser is three units long and the Submarine is two units long.\n"
   board.render
   puts "\n\nEnter the squares for the Cruiser (3 spaces):"
   cruiser = Ship.new("Cruiser",3)
@@ -35,6 +36,7 @@ if play_mode == 'p'
     cruiser_input = gets.chomp.split.map {|str| str.capitalize}
   end
 
+## BUILDING USER BOARD WITH SUBMARINE
   system("clear")
   puts "Player cruiser set at: " + cruiser_input.join(", ")
   board.place(cruiser,cruiser_input)
@@ -47,13 +49,28 @@ if play_mode == 'p'
     puts "Those are invalid coordinates. Please try again:"
     submarine_input = gets.chomp.split.map {|str| str.capitalize}
   end
-
+  
+## BUILDING USER BOARD WITH SUBMARINE
   system("clear")
   puts "Player submarine set at: " + submarine_input.join(", ")
   board.place(submarine,submarine_input)
   puts "\n==============PLAYER BOARD=============="
   board.render(true)
   
+## 2 SHIPS CREATED ON USER BOARD
+## COMPUTER BOARD BUILDING
+  computer_board = Board.new
+  if input_board_dimensions == "\n"
+    computer_board.board_cells
+  else
+    input_board_dimensions = (input_board_dimensions.chomp.sub("x"," ").split).map {|str| str.to_i}
+    computer_board.board_cells(input_board_dimensions[0],input_board_dimensions[1])
+  end
+
+##COMPUTER SHIP PLACEMENTS
+  
+
+## HEALTH TRACKING (GAME CONDITIONALS) 
 
 elsif play_mode == 'q'
   puts "You have quit the game."
